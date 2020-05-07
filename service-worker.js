796ed749ea733fdb28f64a975f40fab8799b0bec
Tata2222 <tataolcha@gmail.com>
2020-05-07 21:12:31 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.237993ceab8c171497bfb95469a06636.js"
+  "./precache-manifest.db28856963a0dc774f9fd03b24ea195c.js"
 );
 
 self.addEventListener('message', (event) => {
